fd007a23931206bc8c521d3e77fe02cbfb27e215
Josue Martinez <129114442+aerojosue@users.noreply.github.com>
2025-08-31 23:24:16 -0300

    Updated PlannedIncomesTable.tsx

--- src/components/incomes/PlannedIncomesTable.tsx
+++ src/components/incomes/PlannedIncomesTable.tsx
@@ -333,7 +333,6 @@ export const PlannedIncomesTable: React.FC<Props> = ({
                   Esta y siguientes
                 </button>
               </div>
-                  type="button"
             </div>
           </div>
         </div>
